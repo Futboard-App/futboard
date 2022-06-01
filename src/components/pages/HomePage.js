07a7d2f9d084f082128b79a8eb4a0f76cd3d904c
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-01 15:34:58 -0700

    added a dropdown to change widget to show either standings, fixtures, or leaderboards on HomePage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -20,6 +20,14 @@ export default function HomePage() {
   const [leagues, setLeagues] = useState([]);
   const [leagueId, setLeagueId] = useState(currentProfile.favorite_league);
   const { push } = useHistory();
+  const viewOptions = [
+    'Standings',
+    'Matches',
+    'Goals Leaderboard',
+    'Assists Leaderboard',
+    'Cards Leaderboard',
+  ];
+  const [view, setView] = useState('Standings');
 
   async function getAllLeagueNames(leagueId) {
     const response = await getLeagueById(leagueId);
@@ -37,11 +45,16 @@ export default function HomePage() {
     setLeagueId(e.target.value);
   }
 
+  function handleViewChange(e) {
+    setView(viewOptions[e.target.value]);
+  }
+
   return (
     <div>
       <Header />
       <div className="fixturesContainer">
         <div className="select-container">
+          {/* select league */}
           <Select
             onChange={(e) => handleLeagueChange(e)}
             defaultValue={currentProfile.favorite_league}
@@ -54,34 +67,103 @@ export default function HomePage() {
               );
             })}
           </Select>
-          <Select
-            onChange={(e) => handleLeagueChange(e)}
-            defaultValue={currentProfile.favorite_league}
-          >
-            {leagues.map((league) => {
+          {/* select view */}
+          <Select onChange={(e) => handleViewChange(e)} defaultValue={0}>
+            {viewOptions.map((viewOption, index) => {
               return (
-                <MenuItem key={league.league_id} value={league.league_id}>
-                  {league.league_name}
+                <MenuItem key={viewOption} value={index}>
+                  {viewOption}
                 </MenuItem>
               );
             })}
           </Select>
         </div>
-        <BroadageWidget
-          requiredFields={{ tournamentId: leagueId }}
-          options={{
-            webNotification: true,
-            sportFilter: false,
-            regionalMatchViewType: 'american',
-            teamRedirectUrl: '/team/{teamId}',
-          }}
-          widget="soccerStandings"
-          bundleId="soccer-st"
-          accountId="0c3f42cf-082d-4d23-a935-660b656c78ee"
-          queryStringParse={{ tournamentId: 'tid' }}
-          className="widget-wrapper"
-        />
-
+        {/* standings view */}
+        {view === 'Standings' && (
+          <BroadageWidget
+            requiredFields={{ tournamentId: leagueId }}
+            options={{
+              webNotification: true,
+              sportFilter: false,
+              regionalMatchViewType: 'american',
+              teamRedirectUrl: '/team/{teamId}',
+            }}
+            widget="soccerStandings"
+            bundleId="soccer-st"
+            accountId="0c3f42cf-082d-4d23-a935-660b656c78ee"
+            queryStringParse={{ tournamentId: 'tid' }}
+            className="widget-wrapper"
+          />
+        )}
+        {/* matches view */}
+        {view === 'Matches' && (
+          <BroadageWidget
+            requiredFields={{ tournamentId: leagueId }}
+            options={{
+              webNotification: true,
+              sportFilter: false,
+              regionalMatchViewType: 'american',
+              matchInfo: 'below',
+              teamRedirectUrl: '/team/{teamId}',
+            }}
+            widget="soccerFixture"
+            bundleId="soccer-fx"
+            accountId="0c3f42cf-082d-4d23-a935-660b656c78ee"
+            queryStringParse={{ tournamentId: 'tid' }}
+            className="widget-wrapper"
+          />
+        )}
+        {/* goals leaderboard view */}
+        {view === 'Goals Leaderboard' && (
+          <BroadageWidget
+            requiredFields={{ tournamentId: leagueId }}
+            options={{
+              webNotification: true,
+              sportFilter: false,
+              regionalMatchViewType: 'american',
+              teamRedirectUrl: '/team/{teamId}',
+            }}
+            widget="soccerLeaderboardGoals"
+            bundleId="soccer-lbg"
+            accountId="0c3f42cf-082d-4d23-a935-660b656c78ee"
+            queryStringParse={{ tournamentId: 'tid' }}
+            className="widget-wrapper"
+          />
+        )}
+        {/* assists leaderboard view */}
+        {view === 'Assists Leaderboard' && (
+          <BroadageWidget
+            requiredFields={{ tournamentId: leagueId }}
+            options={{
+              webNotification: true,
+              sportFilter: false,
+              regionalMatchViewType: 'american',
+              teamRedirectUrl: '/team/{teamId}',
+            }}
+            widget="soccerLeaderboardAssists"
+            bundleId="soccer-lba"
+            accountId="0c3f42cf-082d-4d23-a935-660b656c78ee"
+            queryStringParse={{ tournamentId: 'tid' }}
+            className="widget-wrapper"
+          />
+        )}
+        {/* cards leaderboard view */}
+        {view === 'Cards Leaderboard' && (
+          <BroadageWidget
+            requiredFields={{ tournamentId: leagueId }}
+            options={{
+              webNotification: true,
+              sportFilter: false,
+              regionalMatchViewType: 'american',
+              teamRedirectUrl: '/team/{teamId}',
+            }}
+            widget="soccerLeaderboardCards"
+            bundleId="soccer-lbc"
+            accountId="0c3f42cf-082d-4d23-a935-660b656c78ee"
+            queryStringParse={{ tournamentId: 'tid' }}
+            className="widget-wrapper"
+          />
+        )}
         <LeagueFixtures leagueId={leagueId} />
       </div>
     </div>
