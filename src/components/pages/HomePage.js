8d8dfd2226c40cfffff1ae0e3741cd97a3ee5bd5
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-31 18:02:15 -0700

    got form select to show but having trouble with showing data

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,6 +2,8 @@ import { useStateContext } from '../../StateProvider';
 import { useState, useEffect } from 'react';
 import { MenuItem, Select } from '@mui/material';
 import { getLeagueById } from '../../services/supabase-utils';
+import { useHistory } from 'react-router-dom';
+import LeagueFixtures from '../widgets/LeagueFixtues.js';
 
 import Header from '../Header';
 import './HomePage.scss';
@@ -9,45 +11,50 @@ import './HomePage.scss';
 export default function HomePage() {
   const { currentUser, currentProfile, searchQuery, setSearchQuery } = useStateContext();
   const [leagues, setLeagues] = useState([]);
-  const [leagueNames, setLeagueNames] = useState([]);
-  
-  console.log(leagueNames);
+  const [leagueId, setLeagueId] = useState('');
+  const { push } = useHistory();
 
   async function getAllLeagueNames(leagueId) {
     const response = await getLeagueById(leagueId);
-    return response;
-
+    leagues.push(response);
+    setLeagues([...leagues]);
   }
   useEffect(() => {
-    setLeagues(currentProfile.followed_leagues);
-    setLeagueNames(currentProfile.followed_leagues.map(league => {
+    currentProfile.followed_leagues.map((league) => {
       getAllLeagueNames(league);
-    }));
-  }, []);
+    });
+  }, [leagueId]);
 
+  function handleLeagueChange(e) {
+    setLeagueId(e.target.value);
+  }
+
+  console.log(leagueId);
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="fixturesContainer">
-    hi
-        <Select>
-          {leagueNames.map(leagueName => {
+        hi
+        <Select onChange={(e) => handleLeagueChange(e)}>
+          {leagues.map((league) => {
             return (
-              <option key={leagueName} value={leagueName}>
-                {leagueName}
-              </option>
+              <MenuItem key={league.league_id} value={league.league_id}>
+                {league.league_name}
+              </MenuItem>
             );
           })}
         </Select>
-        <div id="wg-api-football-fixtures2"
+        {/* <div
+          id="wg-api-football-fixtures"
           data-host="v3.football.api-sports.io"
           data-refresh="60"
-          data-league='140'
+          data-league={leagueId}
           data-season="2021"
           data-key={process.env.REACT_APP_API_FOOTBALL_KEY}
           data-show-errors="true"
-          className="api_football_loader fixtures">
-        </div>
+          className="api_football_loader fixtures"
+        /> */}
+        <LeagueFixtures leagueId={leagueId} />
       </div>
     </div>
   );
